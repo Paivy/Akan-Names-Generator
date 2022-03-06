bc3b6f4e837f779410f1c6b4207bc8011a864e32
Paivy <paivyeshirera@gmail.com>
2022-03-06 16:55:31 +0300

    Added an Array of Birthdays in accordance to the name

---
+++ index.js
@@ -0,0 +1,10 @@
+//This is a function used to refresh the form after it gets name
+function formReset(){
+    document.getElementById('form-data').reset();
+}
+
+function akanForm(){
+    var gender = document.getElementById('gender').value;
+    var birthday = document.getElementById('birthday').value;
+    
+}
